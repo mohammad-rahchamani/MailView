926458f6dd09ebc7907e8fb1714432fc534646a6
Mohammad Rahchamani <mohammad.rahchamani@gmail.com>
2020-06-25 03:58:56 +0430

    inject all fields

--- MailView/MailView.swift
+++ MailView/MailView.swift
@@ -12,18 +12,47 @@ import SwiftUI
 
 public struct MailView: UIViewControllerRepresentable {
     
+    public typealias AttachmentData = (Data, String, String)
+    
     @Binding var isShowing: Bool
     
     let subject: String
-    let recipients: [String]?
+    
+    let toRecipients: [String]?
+    let ccRecipients: [String]?
+    let bccRecipients: [String]?
+    
+    let messageBody: String
+    let isHtml: Bool
+    
+    let attachments: [AttachmentData]?
+    
+    let preferredSendingAddress: String
     
     // MARK: init
     public init(isShowing: Binding<Bool>,
-                recipients: [String]? = nil,
-                subject: String = "") {
+                subject: String = "",
+                toRecipients: [String]? = nil,
+                ccRecipients: [String]? = nil,
+                bccRecipients: [String]? = nil,
+                messageBody: String = "",
+                isHtml: Bool = false,
+                attachments: [AttachmentData]? = nil,
+                preferredSendingAddress: String = "") {
         self._isShowing = isShowing
-        self.recipients = recipients
+        
         self.subject = subject
+        
+        self.toRecipients = toRecipients
+        self.ccRecipients = ccRecipients
+        self.bccRecipients = bccRecipients
+        
+        self.messageBody = messageBody
+        self.isHtml = isHtml
+        
+        self.attachments = attachments
+        
+        self.preferredSendingAddress = preferredSendingAddress
     }
     
     public func makeCoordinator() -> Coordinator {
@@ -34,8 +63,25 @@ public struct MailView: UIViewControllerRepresentable {
     public func makeUIViewController(context: Context) -> MFMailComposeViewController {
         let viewController = MFMailComposeViewController()
         viewController.mailComposeDelegate = context.coordinator
-        viewController.setToRecipients(self.recipients)
+        
         viewController.setSubject(self.subject)
+        
+        viewController.setToRecipients(self.toRecipients)
+        viewController.setCcRecipients(self.ccRecipients)
+        viewController.setBccRecipients(self.bccRecipients)
+        
+        viewController.setMessageBody(self.messageBody,
+                                      isHTML: self.isHtml)
+        
+        viewController.setPreferredSendingEmailAddress(self.preferredSendingAddress)
+        
+        for attachment in self.attachments ?? [] {
+            viewController.addAttachmentData(attachment.0,
+                                             mimeType: attachment.1,
+                                             fileName: attachment.2)
+        }
+        
+        
         //
         return viewController
     }
