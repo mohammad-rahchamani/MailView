04ebbc80804daf6bf331ab129ce31529a2582d5b
Mohammad Rahchamani <mohammad.rahchamani@gmail.com>
2020-06-25 02:37:26 +0430

    Initial Commit

---
+++ MailView/MailView.h
@@ -0,0 +1,19 @@
+//
+//  MailView.h
+//  MailView
+//
+//  Created by Mohammad Rahchamani on 4/5/1399 AP.
+//  Copyright © 1399 AP BetterSleep. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MailView.
+FOUNDATION_EXPORT double MailViewVersionNumber;
+
+//! Project version string for MailView.
+FOUNDATION_EXPORT const unsigned char MailViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MailView/PublicHeader.h>
+
+
