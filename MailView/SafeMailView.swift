a81df75c6785d3808dc1028aa83bef9c1b6a0c65
Mohammad Rahchamani <mohammad.rahchamani@gmail.com>
2020-06-25 04:01:02 +0430

    inject fields to safe mail view

--- MailView/SafeMailView.swift
+++ MailView/SafeMailView.swift
@@ -15,18 +15,55 @@ public struct SafeMailView: View {
     
     let subject: String
     
+    let toRecipients: [String]?
+    let ccRecipients: [String]?
+    let bccRecipients: [String]?
+    
+    let messageBody: String
+    let isHtml: Bool
+    
+    let attachments: [AttachmentData]?
+    
+    let preferredSendingAddress: String
+    
+    // MARK: init
     public init(isShowing: Binding<Bool>,
-                subject: String = "") {
+                subject: String = "",
+                toRecipients: [String]? = nil,
+                ccRecipients: [String]? = nil,
+                bccRecipients: [String]? = nil,
+                messageBody: String = "",
+                isHtml: Bool = false,
+                attachments: [AttachmentData]? = nil,
+                preferredSendingAddress: String = "") {
         self._isShowing = isShowing
+        
         self.subject = subject
         
+        self.toRecipients = toRecipients
+        self.ccRecipients = ccRecipients
+        self.bccRecipients = bccRecipients
+        
+        self.messageBody = messageBody
+        self.isHtml = isHtml
+        
+        self.attachments = attachments
+        
+        self.preferredSendingAddress = preferredSendingAddress
     }
     
     public var body: some View {
         Group {
             if MailView.canSendMail {
                 MailView(isShowing: self.$isShowing,
-                         subject: self.subject)
+                         subject: self.subject,
+                         toRecipients: self.toRecipients,
+                         ccRecipients: self.ccRecipients,
+                         bccRecipients: self.bccRecipients,
+                         messageBody: self.messageBody,
+                         isHtml: self.isHtml,
+                         attachments: self.attachments,
+                         preferredSendingAddress: self.preferredSendingAddress)
             } else {
                 Text("test")
             }
