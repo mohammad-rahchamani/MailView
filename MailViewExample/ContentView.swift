296de39488c71880724d7ceb70c15d0913f57048
Mohammad Rahchamani <mohammad.rahchamani@gmail.com>
2020-06-25 02:51:55 +0430

    add new target for showcase app

---
+++ MailViewExample/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MailViewExample
+//
+//  Created by Mohammad Rahchamani on 4/5/1399 AP.
+//  Copyright © 1399 AP BetterSleep. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
