5be9696aea9d63dee00383f5085a124952089b75
Mohammad Rahchamani <mohammad.rahchamani@gmail.com>
2020-06-25 03:39:38 +0430

    add delegate and isShowing binding

--- MailView/MailView.swift
+++ MailView/MailView.swift
@@ -12,38 +12,62 @@ import SwiftUI
 
 public struct MailView: UIViewControllerRepresentable {
     
+    @Binding var isShowing: Bool
+    
     let subject: String
     let recipients: [String]?
     
-    public init(recipients: [String]? = nil,
+    // MARK: init
+    public init(isShowing: Binding<Bool>,
+                recipients: [String]? = nil,
                 subject: String = "") {
+        self._isShowing = isShowing
         self.recipients = recipients
         self.subject = subject
     }
     
     public func makeCoordinator() -> Coordinator {
-        Coordinator()
+        Coordinator(isShowing: self.$isShowing)
     }
     
+    // MARK: make view controller
     public func makeUIViewController(context: Context) -> MFMailComposeViewController {
         let viewController = MFMailComposeViewController()
+        viewController.mailComposeDelegate = context.coordinator
         viewController.setToRecipients(self.recipients)
         viewController.setSubject(self.subject)
         //
         return viewController
     }
     
+    // MARK: update view controller
     public func updateUIViewController(_ uiViewController: MFMailComposeViewController, context: Context) {
         
     }
     
+    // MARK: coordinator
     public class Coordinator: NSObject {
+        
+        @Binding var isShowing: Bool
+        
+        init(isShowing: Binding<Bool>) {
+            self._isShowing = isShowing
+        }
         
     }
     
 }
 
-// MARK: extension
+// MARK: coordinator extension
+extension MailView.Coordinator: MFMailComposeViewControllerDelegate {
+    
+    public func mailComposeController(_ controller: MFMailComposeViewController, didFinishWith result: MFMailComposeResult, error: Error?) {
+        self.isShowing = false
+    }
+    
+}
+
+// MARK: mail view extension
 extension MailView {
     
     public static var canSendMail: Bool {
